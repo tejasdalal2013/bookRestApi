ad596de8e2899314c6a7f80de595cdee05fc3f61
Your Username <you@example.com>
2021-03-14 17:11:24 +0530

    Second Stage

--- models/books.js
+++ models/books.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Author = require('./author')
+const yup = require('yup');
 
 // Book Schema
 
@@ -16,7 +17,22 @@ const BookSchema = new mongoose.Schema({
         required: true,
         minlength: 3,
         maxlength: 20
-    }
-})
+    }  
+}) 
+const validateBook = book => {
+    const schema = yup.object().shape({
+        bookName: yup.string().required().min(3).max(50),
+        authorName: yup.string().required().min(3).max(40),
+        authorAge: yup.number().required().min(10).max(100),
+        genre: yup.string().required().min(3).max(20),
+    });
 
-module.exports = new mongoose.model('Book', BookSchema);
+    return schema.validate(book).then(book => book).catch(error =>{
+        return{
+            message:error.message
+        }
+    });
+
+}
+exports.Book = new mongoose.model('Book', BookSchema);
+exports.validateBook = validateBook
